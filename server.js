f5c9933ad3c3ff1d1c86806ccaa0b00222325d32
unknown <jswon0712@hanmail.net>
2020-08-11 17:06:33 +0900

    132

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express')
 var app = express()
 
-app.use(express.static('./dist/prgDemo'));
+app.use(express.static('./dist/ngxAdminDemo'));
 app.get('/*', function (req, res) {
-    res.sendFile('index.html', { root: 'dist/prgDemo/' }
+    res.sendFile('index.html', { root: 'dist/ngxAdminDemo/' }
     );
 });
 app.listen(process.env.PORT || 8080);
